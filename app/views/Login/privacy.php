446a80264ac90685aae328517b4c50d66c6915b0
kimiron <kirillmironov_97@outlook.com>
2017-11-06 15:41:40 -0500

    1

---
+++ app/views/Login/privacy.php
@@ -0,0 +1,6 @@
+<form action="/privacySettings" method="post">
+Privacy: <select name="privacy">
+			<option value="private">Private</option>
+			<option value="public">Public</option>
+		 </select>
+</form>
--- app/views/Login/signup.php
+++ app/views/Login/signup.php
@@ -1,8 +1,11 @@
 <form method="post" action="/Login/signup" class="form-horizontal">
-username: <input type="text" name="username"><br>
+Username: <input type="text" name="username"><br>
 E-mail: <input type="text" name="email"><br>
-password: <input type="password" name="password"><br>
+Password: <input type="password" name="password"><br>
 Date of birth: <input type="date" name="dob"><br>
-Privacy setting: <input type="number" name="ps"><br>
+Privacy setting: <select name="privacy">
+					<option value="private">Private</option>
+					<option value="public">Public</option>
+				 </select><br>
 <input type="submit" class="btn btn-default" name="action" value="Register" />
 </form>
