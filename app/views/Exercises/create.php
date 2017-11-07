61c8fdabd2be9af562b501e7bdf6c32fd7ecdb88
kimiron <33329310+kimiron@users.noreply.github.com>
2017-11-07 14:42:35 -0500

    more

--- app/views/Exercises/create.php
+++ app/views/Exercises/create.php
@@ -1,5 +1,13 @@
 <form method="post" action="/Exercise_controller/createExercise" class="form-horizontal">
 Title: <input type="text" name="title"><br>
-Muscle group: <input type="text" name="muscle_group_id"><br>
+Muscle group: <select name="muscleGroup">		  
+<?php
+$exercise = $this->model('muscle_group');
+$results = $exercise->get();
+foreach($results as $group ){
+	echo '<option value="'.$group->muscle_group_id. '"/>'.$group->muscle_group_name.'</option>';
+}
+?>
+</select><br>
 <input type="submit" class="btn btn-default" name="action" value="Create" />
 </form>
--- app/views/Workout/create.php
+++ app/views/Workout/create.php
@@ -1,5 +1,7 @@
 <form method="post" action="/Workout_controller/createWorkout" class="form-horizontal">
 Title: <input type="text" name="title"><br>
 Muscle group: <input type="text" name="muscle_group_id"><br>
+Position: <input type="text" name="position"><br>
+Number of sets:
 <input type="submit" class="btn btn-default" name="action" value="Create" />
 </form>
