a76fe872d691378b180df053de58a0b7b0c944f8
kimiron <33329310+kimiron@users.noreply.github.com>
2017-11-27 15:27:17 -0500

    jhgjgh

--- app/views/Users/personal_record.php
+++ app/views/Users/personal_record.php
@@ -10,7 +10,7 @@ if($data['records'] == null)
 	echo "You dont have any personal records yet";
 else{
 	foreach($data['records'] as $item){
-		echo "Exercise: " . $item->record_id . "<br>";
+		echo "Exercise: " . Helpers::getExerciseTitle($item->record_id) . "<br>";
 		echo "Record: " . $item->record . "<br><form method=\"get\" action=\"\" class=\"form-horizontal\">
 			<input type=\"submit\" class=\"btn btn-default\" name=\"action\" value=\"Update Record\">
 			<input type=\"hidden\" name=\"record_id\" value=\"". $item->record_id . "\">
